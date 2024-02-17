fdf2d76625793f98e67c36dfff6dbf4a44b4cf99
mhmd <ms.mhmdsalah@gmail.com>
2024-02-17 18:19:43 +0200

    commit num-2

--- main.js
+++ main.js
@@ -5,4 +5,28 @@ let sepia = document.getElementById("sepia")
 let grayscale = document.getElementById("grayscale")
 let blur = document.getElementById("blur")
 let hueRotate = document.getElementById("hue-rotate")
-console.log(saturate)
+let upload = document.getElementById( "upload" );
+let download = document.getElementById( "download" );
+let img = document.getElementById("img")
+let reset = document.querySelector('span')
+let imgBox = document.querySelector(".img-box")
+//------------------------------------------------------
+
+window.onload = function () {
+  download.style.display = "none"
+  reset.style.display = "none"
+  imgBox.style.display = "none"
+};
+upload.onchange = function () {
+  download.style.display = "block"
+  reset.style.display = "block"
+  imgBox.style.display = "block"
+  let file = new FileReader();
+  file.readAsDataURL( upload.files[ 0 ] )
+  file.onload = function () {
+    img.src = file.result;
+  }
+};
+saturate.addEventListener( "input", function () {
+  img.style.filter = `saturate(${saturate.value}%)`
+})
